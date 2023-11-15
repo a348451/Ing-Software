9e98df63f7da16524d0815e441433ee3da18b361
RamonPloP <aspireandlive@gmail.com>
2023-11-14 23:27:51 -0600

    Terminacio de estructura y backend

--- models/menu.js
+++ models/menu.js
@@ -1,17 +1,3 @@
-module.exports = (sequelize, type) =>{
-    const Menu = sequelize.define('menus',{
-        id:{
-            type: type.INTEGER,
-            autoIncrement: true,
-            primaryKey: true
-        },
-        name: type.STRING,
-        prize: type.FLOAT,
-        disponibility: type.BOOLEAN
-    });
-    return Menu;
-}
-
 const mongoose = require('mongoose');
 
 const schema = mongoose.Schema({
